6fe4c88c2dc2b6982495afef6e7f53b9ef67a0fd
Teddy Horvath <teddyhorvath@Teddys-MacBook-Pro.local>
2022-09-21 17:52:39 -0700

    merging

--- project-root/src/pages/index.js
+++ project-root/src/pages/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import HeroSection from "../components/HeroSection";
-import Navbar from "../components/Navbar";
-import Footer from "../../src/components/Footer";
-import Products from "../components/Products";
-import InfoSection from "../components/InfoSection";
-import { homeObjOne, homeObjTwo } from "../components/InfoSection/Data";
-import Testimonials from "../components/Testimonials";
-import Sidebar from "../components/Sidebar";
-import ContactUs from "../components/ContactUs";
-=======
 import React, {useState} from 'react'
 import HeroSection from '../components/HeroSection'
 import Navbar from '../components/Navbar'
@@ -20,7 +8,8 @@ import { homeObjOne, homeObjTwo } from '../components/InfoSection/Data'
 import Testimonials from '../components/Testimonials'
 import Sidebar from '../components/Sidebar'
 import Facilities from '../components/Facilities '
->>>>>>> morgan-dev
+import ContactUs from "../components/ContactUs";
+
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,14 +24,9 @@ const Home = () => {
       <Navbar toggle={toggle} />
       <HeroSection />
       <Products />
-<<<<<<< HEAD
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
-=======
-      <InfoSection {...homeObjOne}/>
-      <InfoSection {...homeObjTwo}/>
       <Facilities />
->>>>>>> morgan-dev
       <Testimonials />
       <ContactUs />
       <Footer />
