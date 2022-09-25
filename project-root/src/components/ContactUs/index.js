9c29adf4589bae4984fe646bc2ae599bd87d2c1b
Odysseas Fourakis <odysseasf@me.com>
2022-09-25 16:26:39 -0700

    Adding DB stuff

--- project-root/src/components/ContactUs/index.js
+++ project-root/src/components/ContactUs/index.js
@@ -67,6 +67,24 @@ class ContactUs extends React.Component {
     event.preventDefault();
   }
 
+  backendData = [
+    {
+      title: "Grocery List",
+      description: "Milk, Soup, Bread",
+      createdat: "01-18-2021",
+    },
+    {
+      title: "Math Homework",
+      description: "Remember to finish question 8-10 before monday",
+      createdat: "12-01-2020",
+    },
+    {
+      title: "Call James",
+      description: "Ask him about the company party.",
+      createdat: "12-30-2020",
+    },
+  ];
+
   render() {
     return (
       <ServicesContainer id="contact">
